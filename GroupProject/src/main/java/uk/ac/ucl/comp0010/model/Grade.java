8d573877d11ba326741ced1ed7b88a160a53f8dd
Kevin <zcabinf@ucl.ac.uk>
2024-11-16 15:14:14 +0000

    new Grade Class with a fixing bug

--- GroupProject/src/main/java/uk/ac/ucl/comp0010/model/Grade.java
+++ GroupProject/src/main/java/uk/ac/ucl/comp0010/model/Grade.java
@@ -3,7 +3,9 @@ package uk.ac.ucl.comp0010.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "grade")
@@ -12,6 +14,7 @@ public class Grade {
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private int id;
+
   private int score;
 
   @ManyToOne
